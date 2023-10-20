3c71110626909cdd1ae4067fdc68a3091d5e4746
Sparrow <2196442691@qq.com>
2023-10-20 20:37:25 +0800

    python Template

---
+++ CODE/DSU.py
@@ -0,0 +1,26 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [1] * n
+        self.n = n
+        self.setCount = n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        x, y = self.find(x), self.find(y)
+        if x == y:
+            return False
+        if self.size[x] < self.size[y]:
+            x, y = y, x
+        self.parent[y] = x
+        self.size[x] += self.size[y]
+        self.setCount -= 1
+        return True
+
+    def connected(self, x, y):
+        x, y = self.find(x), self.find(y)
+        return x == y
